41b7c3f98ab781810af658d598beab88f618eaac
Volodymyr Sushyk <vsushyk13@gmail.com>
2014-07-15 13:45:11 -0700

    This is my first commit

---
+++ thing.py
@@ -0,0 +1,13 @@
+def my_func(self):
+	first_name = "Volodymyr";
+	last_name = "Sushyk";
+	print ("%s" + " " + "%s") % (first_name, last_name);
+
+def new(__init__):
+	func = my_func();
+
+first_name = "Vlad";
+last_name = "Sushyk";
+print ("%s" + " " + "%s") % (first_name, last_name);
+print "My name is %s" % first_name;
+
